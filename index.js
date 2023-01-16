d6d66ad48b1fa178e48577ff5d742a0eba064b54
Julien L <julien_lemoing@yahoo.fr>
2023-01-16 16:58:02 +0100

    sss

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ try {
   const jsonFolder = core.getInput('jsonFolder');
   const rootFolder = process.env.GITHUB_WORKSPACE
   //core.getInput('rootAssetsFolder');
-  console.log(`jsonFolder ${jsonFolder}!`);
-  console.log(`rootFolder ${rootFolder}!`);
+  core.info(`jsonFolder ${jsonFolder}!`);
+  core.info(`rootFolder ${rootFolder}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
@@ -18,22 +18,22 @@ try {
   fs.readdir(directoryPath, function (err, files) {
       //handling error
       if (err) {
-          return console.log('Unable to scan directory: ' + err);
+          return core.info('Unable to scan directory: ' + err);
       } 
       //listing all files using forEach
       files.forEach(function (file) {
           // Do whatever you want to do with the file
-          console.log(file); 
+          core.info(file); 
 
           
           let rawdata = fs.readFileSync(file);
           let jsonFile = JSON.parse(rawdata);
-          console.log(jsonFile[0].files.length);
+          core.info(jsonFile[0].files.length);
           for(let i=0;i<jsonFile[0].files.length;i++){
-            console.log(jsonFile[0].files[i].path);
+            core.info(jsonFile[0].files[i].path);
             try {
               if (fs.existsSync(path.join(rootFolder,jsonFile[0].files[i].path))) {
-                console.log("File found " + jsonFile[0].files[i].path + " referenced by file : " + file);
+                core.info("File found " + jsonFile[0].files[i].path + " referenced by file : " + file);
               }
             } catch(err) {
               core.setFailed("File not found : " + jsonFile[0].files[i].path + " referenced by file : " + file);
@@ -46,7 +46,7 @@ try {
 
 
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
+  // core.info(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
