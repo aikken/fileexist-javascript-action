4a43f63d6a96aaf0e4af5f633e627594fe65926c
Julien L <julien_lemoing@yahoo.fr>
2023-01-17 11:40:27 +0100

    sdc

--- dist/index.js
+++ dist/index.js
@@ -6441,6 +6441,29 @@ function onceStrict (fn) {
 
 /***/ }),
 
+/***/ 1139:
+/***/ ((module, exports, __nccwpck_require__) => {
+
+"use strict";
+
+var fs = __nccwpck_require__(7147);
+var path = __nccwpck_require__(1017);
+
+module.exports = exports = function readdir(dir, cb) {
+  fs.readdir(dir, function (err, files) {
+    cb(err, files && files.map(join(dir)));
+  });
+};
+
+function join (dir) {
+  return function (file) {
+    return path.join(dir, file);
+  };
+}
+
+
+/***/ }),
+
 /***/ 9427:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -9776,37 +9799,40 @@ const core = __nccwpck_require__(6024);
 const github = __nccwpck_require__(5016);
 const path = __nccwpck_require__(1017);
 const fs = __nccwpck_require__(7147);
+var readdir = __nccwpck_require__(1139);
 try {
 
   const jsonFolder = core.getInput('jsonFolder');
-  const rootAssetsFolder = core.getInput('rootAssetsFolder');
-  console.log(`jsonFolder ${jsonFolder}!`);
-  console.log(`rootAssetsFolder ${rootAssetsFolder}!`);
+  const rootFolder = process.env.GITHUB_WORKSPACE
+  //core.getInput('rootAssetsFolder');
+  core.info(`jsonFolder ${jsonFolder}!`);
+  core.info(`rootFolder ${rootFolder}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
 
-  const directoryPath = path.join(__dirname, jsonFolder);
+  //const directoryPath = path.join(jsonFolder);
+  //core.info(`directoryPath ${directoryPath}!`);
   //passsing directoryPath and callback function
-  fs.readdir(directoryPath, function (err, files) {
+  readdir(path.resolve(jsonFolder), function (err, files) {
       //handling error
       if (err) {
-          return console.log('Unable to scan directory: ' + err);
+          return core.info('Unable to scan directory: ' + err);
       } 
       //listing all files using forEach
       files.forEach(function (file) {
           // Do whatever you want to do with the file
-          console.log(file); 
+          core.info(file); 
 
           
           let rawdata = fs.readFileSync(file);
           let jsonFile = JSON.parse(rawdata);
-          console.log(jsonFile[0].files.length);
+          core.info(jsonFile[0].files.length);
           for(let i=0;i<jsonFile[0].files.length;i++){
-            console.log(jsonFile[0].files[i].path);
+            core.info(jsonFile[0].files[i].path);
             try {
-              if (fs.existsSync(jsonFile[0].files[i].path)) {
-                console.log("File found " + jsonFile[0].files[i].path + " referenced by file : " + file);
+              if (fs.existsSync(path.join(rootFolder,jsonFile[0].files[i].path))) {
+                core.info("File found " + jsonFile[0].files[i].path + " referenced by file : " + file);
               }
             } catch(err) {
               core.setFailed("File not found : " + jsonFile[0].files[i].path + " referenced by file : " + file);
@@ -9819,7 +9845,7 @@ try {
 
 
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
+  // core.info(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
