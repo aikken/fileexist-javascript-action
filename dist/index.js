695012c28b089dbc5720baad640a03d9710d8866
Julien L <julien_lemoing@yahoo.fr>
2023-01-17 11:53:23 +0100

    2.4

--- dist/index.js
+++ dist/index.js
@@ -9803,7 +9803,7 @@ var readdir = __nccwpck_require__(1139);
 try {
 
   const jsonFolder = core.getInput('jsonFolder');
-  const rootFolder = process.env.GITHUB_WORKSPACE
+  const workspaceFolder = core.getInput('workspaceFolder');
   //core.getInput('rootAssetsFolder');
   core.info(`jsonFolder ${jsonFolder}!`);
   core.info(`rootFolder ${rootFolder}!`);
@@ -9811,35 +9811,39 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
 
-  //const directoryPath = path.join(jsonFolder);
+  const directoryPath = path.join(workspaceFolder, jsonFolder);
   //core.info(`directoryPath ${directoryPath}!`);
   //passsing directoryPath and callback function
-  readdir(path.resolve(jsonFolder), function (err, files) {
-      //handling error
-      if (err) {
-          return core.info('Unable to scan directory: ' + err);
-      } 
-      //listing all files using forEach
-      files.forEach(function (file) {
-          // Do whatever you want to do with the file
-          core.info(file); 
+  readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      return core.info('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+      // Do whatever you want to do with the file
+      core.info(file);
+      if (file.split('.') > 1) {
+        let rawdata = fs.readFileSync(file);
+        let jsonFile = JSON.parse(rawdata);
 
-          
-          let rawdata = fs.readFileSync(file);
-          let jsonFile = JSON.parse(rawdata);
-          core.info(jsonFile[0].files.length);
-          for(let i=0;i<jsonFile[0].files.length;i++){
-            core.info(jsonFile[0].files[i].path);
-            try {
-              if (fs.existsSync(path.join(rootFolder,jsonFile[0].files[i].path))) {
-                core.info("File found " + jsonFile[0].files[i].path + " referenced by file : " + file);
-              }
-            } catch(err) {
-              core.setFailed("File not found : " + jsonFile[0].files[i].path + " referenced by file : " + file);
+        core.info(jsonFile[0].files.length);
+
+        for (let i = 0; i < jsonFile[0].files.length; i++) {
+          core.info(jsonFile[0].files[i].path);
+          try {
+            if (fs.existsSync(path.join(workspaceFolder, jsonFile[0].files[i].path))) {
+              core.info("File found " + jsonFile[0].files[i].path + " referenced by file : " + file);
             }
+          } catch (err) {
+            core.setFailed("File not found : " + jsonFile[0].files[i].path + " referenced by file : " + file);
           }
-        
-      });
+        }
+      }
+      else core.info("Ignore this file because of file pattern :" + file);
+
+
+    });
   });
 
 
